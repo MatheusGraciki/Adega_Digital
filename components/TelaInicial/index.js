e25ceddc66d09e68cdff826f52071d418f90aa65
Matheus Graciki <85004422+MatheusGraciki@users.noreply.github.com>
2022-05-26 21:42:23 -0300

    Create TelaInicial

---
+++ components/TelaInicial/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+
+export default function TelaInicial() {
+    return(
+        <View>
+            <Text>Hello world</Text>
+        </View>
+    )
+}
